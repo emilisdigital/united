0fa6f6b4ceb2e620cd93d6cfedbb496f784f12e4
Emil Bagirov <emilisdigital@gmail.com>
2022-10-27 02:45:18 +0400

    add component Post

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { Post } from './Post';
 
 function App() {
     return (
@@ -8,8 +9,17 @@ function App() {
             <div>developer/login new branch commit 2</div>
             <div>developer/popup new branch commit 1</div>
             <div>developer/popup new branch commit 2</div>
+            <div>
+                111
+                222
+                333
+                444
+            </div>
+            <Post/>
         </div>
     );
 }
 
+
 export default App;
+
